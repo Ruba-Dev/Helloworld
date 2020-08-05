b12d3a7d2125d80c3ed818f879899952bd61a087
Ruba <rub.mujalli@gmail.com>
2020-08-05 19:21:18 +0300

    test commit

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+Public class HelloWorld{
+    string a = "Ruba is a developer!";
+    public static void main(String[] args) {
+        HelloWorld obj = new HelloWorld();
+        System.out.println(obj.a);
+
+    }
+}
+ class ToTheTrain {
+
+ }
